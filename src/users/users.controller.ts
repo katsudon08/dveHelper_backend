b45be194a812e69a8f52ef5529abcde71ca53d60
matsu <matsuaki171101@gmail.com>
2023-05-09 16:09:04 +0900

    controllerの非同期化

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,29 +1,32 @@
 import { Controller, Get, Post, Delete, Put } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+    constructor(private usersService: UsersService) {}
+
     @Get()
-    findAllUser(): string {
+    async findAllUser(): Promise<string> {
         return '全てのユーザー情報を返す';
     }
 
     @Get('find')
-    findUser(): string {
+    async findUser(): Promise<string> {
         return '特定のユーザーアカウントを返す'
     }
 
     @Post('create')
-    createUser(): string {
+    async createUser(): Promise<string> {
         return 'ユーザーを作成し、その結果を返す'
     }
 
     @Delete()
-    deleteUser(): string {
+    async deleteUser(): Promise<string> {
         return 'ユーザーを削除し、その結果を返す'
     }
 
     @Put()
-    updateUser(): string {
+    async updateUser(): Promise<string> {
         return 'ユーザー情報を更新し、その結果を返す'
     }
 }
