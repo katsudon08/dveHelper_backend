af15281e44a5614d7216e7cc523fc5dca4bf9009
matsu <matsuaki171101@gmail.com>
2023-05-09 14:39:34 +0900

    userモジュールでのコントローラーの方針を決定

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Put } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    @Get()
+    findAllUser(): string {
+        return '全てのユーザー情報を返す';
+    }
+
+    @Get('find')
+    findUser(): string {
+        return '特定のユーザーアカウントを返す'
+    }
+
+    @Post('create')
+    createUser(): string {
+        return 'ユーザーを作成し、その結果を返す'
+    }
+
+    @Delete()
+    deleteUser(): string {
+        return 'ユーザーを削除し、その結果を返す'
+    }
+
+    @Put()
+    updateUser(): string {
+        return 'ユーザー情報を更新し、その結果を返す'
+    }
+}
